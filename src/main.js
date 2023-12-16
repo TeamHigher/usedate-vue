e34ef72cafee5e3de24b814c2845469404cbab43
Uwabuofu Chukuka <chuksdavis2020@gmail.com>
2023-12-16 05:36:54 +0000

    third commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 createApp(App).mount("#app");
-import axios from "./axios"; // Import the Axios instance
+
 
 Vue.config.productionTip = false;
 
-Vue.prototype.$axios = axios; // Attach Axios to Vue's prototype
+
 
 new Vue({
   render: (h) => h(App),
