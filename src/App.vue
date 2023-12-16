f6e35f857171717d2b70f00858e9825278cd09ed
Uwabuofu Chukuka <chuksdavis2020@gmail.com>
2023-12-16 05:35:58 +0000

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,47 @@
 <template>
-  <section>
-    <div>New Date: {{ newDate }}</div>
-    <br />
-    <div>New Updated Date: {{ updatedDate }}</div>
-    <br />
-    <div>
-      <div
-        v-for="post in posts"
-        :key="post.id"
-        style="
-          border: 1px dashed;
-          margin-bottom: 5px;
-          margin-left: 5px;
-          width: 400px;
-        "
-      >
-        <p>{{ post.title }}</p>
-        <p>ID: {{ post.id }}</p>
-      </div>
-    </div>
-  </section>
+  <div>
+    <p>Current Date: {{ currentDate }}</p>
+    <p>Current Day: {{ getDay() }}</p>
+    <p>Current Month: {{ getMonth() }}</p>
+  </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      posts: [],
-      newDate: "",
-      updatedDate: "",
-    };
-  },
-  methods: {
-    addDays(date, days) {
-      const newDate = new Date(date);
-      newDate.setDate(newDate.getDate() + days);
-      return newDate;
-    },
-    addMonths(date, months) {
-      const newDate = new Date(date);
-      newDate.setMonth(newDate.getMonth() + months);
-      return newDate;
-    },
-    async fetchData() {
-      try {
-        const response = await this.$axios.get(
-          "https://jsonplaceholder.typicode.com/todos"
-        );
-        const data = response.data.map((item) => {
-          if (item.id === 1) {
-            return {
-              ...item,
-              title: `I just edited this line.`,
-            };
-          } else {
-            return item;
-          }
-        });
-        this.posts = data;
-      } catch (error) {
-        console.error("Error fetching data:", error.message);
-        // Provide user feedback for the error
-      }
-    },
-  },
-  created() {
-    this.newDate = this.addDays("10/10/2023", 12).toLocaleString();
-    const currentDate = new Date("10/10/2023");
-    this.updatedDate = this.addMonths(currentDate, 10).toLocaleString();
+import { ref } from "vue";
 
-    this.fetchData();
-  },
-  beforeDestroy() {
-    console.log("Component is being destroyed");
-    // Add any necessary clean-up tasks here
+export default {
+  setup() {
+    const date = ref(new Date());
+
+    const getDay = () => {
+      return date.value.getDay();
+    };
+
+    const getMonth = () => {
+      return date.value.getMonth() + 1;
+    };
+
+    const addDay = (numberOfDays) => {
+      date.value.setDate(date.value.getDate() + numberOfDays);
+      if (date.value.getMonth() !== getMonth() - 1) {
+        date.value.setDate(1);
+      }
+    };
+
+    const addMonth = (numberOfMonths) => {
+      date.value.setMonth(date.value.getMonth() + numberOfMonths);
+      if (date.value.getMonth() !== (getMonth() - 1 + numberOfMonths) % 12) {
+        date.value.setMonth(0);
+      }
+    };
+
+    return {
+      currentDate: date,
+      getDay,
+      getMonth,
+      addDay,
+      addMonth,
+    };
   },
 };
 </script>
